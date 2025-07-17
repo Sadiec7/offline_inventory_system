3a94f067bde2e3967cb9a1df3b4b76f39701f744
sadiec7 <saldiec7@gmail.com>
2025-07-17 15:03:19 -0600

    Now we can use exports and imports with sqlite

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require('path');
 
 function createWindow() {
@@ -16,4 +16,31 @@ function createWindow() {
   win.loadFile(path.join(__dirname, 'src', 'views', 'login.html'));
 }
 
-app.whenReady().then(createWindow);
+// Registrar el handler IPC para show-save-dialog
+function setupIpcHandlers() {
+  ipcMain.handle('show-save-dialog', async (event, options) => {
+    // Obtenemos la ventana que emitió el mensaje
+    const win = BrowserWindow.fromWebContents(event.sender);
+    // Mostramos el diálogo de guardar
+    const result = await dialog.showSaveDialog(win, options);
+    return result;
+  });
+}
+
+app.whenReady().then(() => {
+  setupIpcHandlers();
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Cierra la app en macOS cuando todas las ventanas se cierran
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
